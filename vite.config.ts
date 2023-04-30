957964eaf9d00185172866f6eac614d11d835887
charlie <lry101@outlook.com>
2023-04-30 19:24:30 +0800

    prods配置文件完成

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { mergeConfig } from 'vite'
+import baseConfig from './config/vite.config.base'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default mergeConfig(baseConfig, {
+	mode: 'dev', // 运行环境变量
+	server: {
+		// 项目部署端口：
+		prot: 8000,
+		host: '0.0.0.0',
+	},
 })
